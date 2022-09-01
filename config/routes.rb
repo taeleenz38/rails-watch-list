1046a1d47339083642af81f3c0c8cb1daf003ceb
Tae Lee <taekyu.lee3@gmail.com>
2022-09-01 12:29:38 +1000

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  resources :lists
-  root "lists#index"
-  # root "articles#index"
+  root to: "lists#index"
+
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
